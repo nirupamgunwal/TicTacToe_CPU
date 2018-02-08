ae185b13e9f62862988db30643feb5f3cd0c514f
Nirupam Gunwal <nirupamgunwal@gmail.com>
2018-02-08 16:45:41 +0530

    Added User Interface to select Cpu or Player

---
+++ app/src/main/java/com/nirupam/myapp7/Main3Activity.java
@@ -0,0 +1,50 @@
+package com.nirupam.myapp7;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main3Activity extends AppCompatActivity {
+
+    private Button button;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+
+
+
+    public void openActivity(){
+        Intent intent1 = new Intent(this , MainActivity.class);
+
+        startActivity(intent1);
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this , Main2Activity.class);
+
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/nirupam/myapp7/MainActivity.java
+++ app/src/main/java/com/nirupam/myapp7/MainActivity.java
@@ -12,11 +12,13 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     //activePlayer = 0 means Cross and 1 means Circle
-    int activePlayer = 0;
+    int activePlayer = -1;
+
+    int count = 0;
 
     boolean gameIsActive = true;
 
-    int[] gameState = {2,2,2,2,2,2,2,2,2};
+    int[] gameState = {0,0,0,0,0,0,0,0,0};
 
     int[][] winningPositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
@@ -29,37 +31,36 @@ public class MainActivity extends AppCompatActivity {
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
 
-        if(gameState[tappedCounter] == 2 && gameIsActive == true) {
+        if(gameState[tappedCounter] == 0 && gameIsActive == true) {
 
             counter.setTranslationY(1100f);
 
             gameState[tappedCounter] = activePlayer;
 
-            if (activePlayer == 0) {
+            if (activePlayer == -1 && count<9) {
 
                 counter.setImageResource(R.drawable.cross);
 
                 activePlayer = 1;
 
-            } else {
+                counter.animate().translationYBy(-1100f).setDuration(300);
 
-                counter.setImageResource(R.drawable.circle);
+                count++;
 
-                activePlayer = 0;
+                cpumove();
+
             }
-
-            counter.animate().translationYBy(-1100f).setDuration(300);
 
             for( int[] winningPosition : winningPositions){
                 if(gameState[winningPosition[0]] == gameState[winningPosition[1]]
                         && gameState[winningPosition[1]] == gameState[winningPosition[2]]
-                        && gameState[winningPosition[2]] != 2) {
+                        && gameState[winningPosition[2]] != 0) {
 
                     gameIsActive = false;
 
                     TextView textView = (TextView) findViewById(R.id. winningGameText);
 
-                    if (gameState[winningPosition[0]] == 0) {
+                    if (gameState[winningPosition[0]] == -1) {
                         textView.setText("Cross Won");
 
 
@@ -83,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                     boolean gameIsOver = true;
 
                     for (int counterState : gameState){
-                        if (counterState == 2){
+                        if (counterState == 0){
                             gameIsOver = false;
                         }
                     }
@@ -113,12 +114,14 @@ public class MainActivity extends AppCompatActivity {
     public void playAgain(View view){
 
 
-        activePlayer = 0;
+        activePlayer = -1;
+
+        count = 0;
 
         gameIsActive = true;
 
         for (int i =0;i<gameState.length; i++){
-            gameState[i] = 2;
+            gameState[i] = 0;
 
         }
 
@@ -134,6 +137,87 @@ public class MainActivity extends AppCompatActivity {
 
         linearLayout.setVisibility(View.INVISIBLE);
 
+
+    }
+
+
+
+    public int win(int[] board) {
+        //determines if a player has won, returns 0 otherwise.
+        int[][]  wins = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
+        int i;
+
+        for(i = 0; i < 8; ++i) {
+            if(board[wins[i][0]] != 0 &&
+                    board[wins[i][0]] == board[wins[i][1]] &&
+                    board[wins[i][0]] == board[wins[i][2]])
+                return board[wins[i][2]];
+        }
+        return 0;
+    }
+
+
+    public int computerMove(int[] board) {
+        int move = -1;
+        int score = -2;
+        int i;
+        for(i = 0; i < 9; ++i) {
+            if(board[i] == 0) {
+                board[i] = 1;
+                int tempScore = -minimax(board, -1);
+                board[i] = 0;
+                if(tempScore > score) {
+                    score = tempScore;
+                    move = i;
+                }
+            }
+        }
+        //returns a score based on minimax tree at a given node.
+        board[move] = 1;
+        return move;
+    }
+
+
+    public int minimax(int[] board, int player) {
+        //How is the position like for player (their turn) on board?
+        int winner = win(board);
+        if(winner != 0) return winner*player;
+
+        int move = -1;
+        int score = -2;//Losing moves are preferred to no move
+        int i;
+        for(i = 0; i < 9; ++i) {//For all moves,
+            if(board[i] == 0) {//If legal,
+                board[i] = player;//Try the move
+                int thisScore = -minimax(board, player*-1);
+                if(thisScore > score) {
+                    score = thisScore;
+                    move = i;
+                }//Pick the one that's worst for the opponent
+                board[i] = 0;//Reset board after try
+            }
+        }
+        if(move == -1) return 0;
+        return score;
+    }
+
+    public void cpumove(){
+
+        if(count < 9) {
+
+            int move;
+            move = computerMove(gameState);
+            gameState[move] = 1;
+            ImageView i1 = (ImageView) findViewById(R.id.gridLayout).findViewWithTag(Integer.toString(move));
+            i1.setTranslationY(1100f);
+            i1.setImageResource(R.drawable.circle);
+            i1.animate().translationYBy(-1100f).setDuration(300);
+
+            activePlayer = -1;
+
+            count++;
+        }
 
     }
 
