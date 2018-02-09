ace2ba42c4f24159a3e0d12c8eda49fce69c05ad
Nirupam Gunwal <nirupamgunwal@gmail.com>
2018-02-09 17:12:07 +0530

    Made Toast for upcoming feature

--- app/src/main/java/com/nirupam/myapp7/Main3Activity.java
+++ app/src/main/java/com/nirupam/myapp7/Main3Activity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Main3Activity extends AppCompatActivity {
 
     private Button button;
     private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class Main3Activity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.button);
         button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +33,13 @@ public class Main3Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 openActivity2();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                devmessage();
             }
         });
     }
@@ -46,5 +56,9 @@ public class Main3Activity extends AppCompatActivity {
         Intent intent = new Intent(this , Main2Activity.class);
 
         startActivity(intent);
+    }
+
+    public void devmessage(){
+        Toast.makeText(this, "Feature not available yet" , Toast.LENGTH_SHORT).show();
     }
 }
